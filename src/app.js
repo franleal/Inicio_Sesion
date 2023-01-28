b44c76f0b00a6dc494713a636c7bea89d8aac728
franleal <franciscoleal152000@gmail.com>
2023-01-28 14:03:13 -0300

    fort and cluster

--- src/app.js
+++ src/app.js
@@ -1,13 +1,32 @@
 require("dotenv").config()
 
 const app = require('./server')
+const cluster = require('cluster')
 require('./database')
-port = 8080
+const port = parseInt(process.argv[2]) || 8080
+const numCPUs = require('os').cpus().length
+
+if (cluster.isMaster && process.argv[2] == 'CLUSTER'){
+    console.log(`Master${process.pid} is running`)
+
+    for (let i = 0; i< numCPUs; i++){
+        cluster.fork()
+    }
+
+    cluster.on('exit', worker =>{
+        console.log(`Worker ${worker.process.pid} died`)
+        cluster.fork()
+    })
+
+}else{
+
+    app.listen(port,() =>{
+        console.log(`Servidor escuchado en http://localhost:${port} - PID${process.pid}`)
+    })
+    app.on('error',err =>{
+        console.log('Error en sercvidor',err)
+    })
+
+}
 
 
-app.listen(port,() =>{
-    console.log(`Servidor escuchado en http://localhost:${port}`)
-})
-app.on('error',err =>{
-    console.log('Error en sercvidor',err)
-})
---
+++ src/controllers/comandos.js
@@ -0,0 +1,11 @@
+const comandosController = {}
+
+comandosController.renderClouster = (req,res)=>{
+    
+
+    res.render('index')
+
+
+}
+
+module.exports = comandosController
